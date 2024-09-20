eb4ae0baed2d635cb59232cf02463f4cbd35a292
azizbek117 <raximovazizbek601@gmail.com>
2024-09-20 15:37:31 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -5,14 +5,14 @@ int main() {
   float H;
   cin >>S>>H;
   if (S=="M" && H<=1.70)
-    cout<<"Short";
-    else if (S=="M" && H>=1.70 || S=="M" && H<=1.85)
+        cout<<"Short";
+    else if (S=="M" && H>=1.70 && S=="M" && H<=1.85)
         cout<<"Normal";
     else if (S=="M" && H>=1.85)
         cout<<"Tall";
     else if (S=="F" && H<=1.60)
         cout<<"Short";
-    else if (S=="F" && H>=1.60|| S=="F" && H<=1.75)
+    else if (S=="F" && H>=1.60 && S=="F" && H<=1.75)
         cout<<"Normal";
     else if (S=="F" && H>=1.75)
         cout<<"Tall";
