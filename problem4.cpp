3992ad1458bd379591eb1b64bfae566151b75c46
azizbek117 <raximovazizbek601@gmail.com>
2024-09-20 15:26:57 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -4,9 +4,11 @@ int main() {
     int n1,n2,n3,n4;
     cin >> n1 >> n2 >> n3 >> n4;
     if (n1==n2 && n2==n3 && n3==n4)
-        cout << 0;
+        cout << 3;
     else if (n1==n2 && n1==n3 && n1!=n4)
         cout << 2;
     else if (n2!=n3 && n2!=n1 && n2==n4)
         cout << 1;
+    else if (n1!=n2 && n2!=n3 && n3!=n4)
+        cout << 0;
 }
