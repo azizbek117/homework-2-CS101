580d1d16ce3ba507e2f09e6f02a0429e1f3b68aa
azizbek117 <raximovazizbek601@gmail.com>
2024-09-20 15:44:53 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -4,13 +4,11 @@ int main() {
     int a,b;
     cin>>a>>b;
 
-    if (a==1 && b==1)
+    if (a==1)
         cout<<"Bus";
     else if (a==0 && b==1)
         cout<<"Walk";
-    else if (a==1 && b==0)
-        cout<<"Bus";
-    else if (a==0 && b==0)
-        cout<<"Walk";
+    else
+        cout<<"Bike";
 
 }
