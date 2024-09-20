201eccc50aebf095a373ebd2fcccbb94c009e720
azizbek117 <raximovazizbek601@gmail.com>
2024-09-20 15:30:56 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -3,10 +3,14 @@ using namespace std;
 int main() {
     int a,b;
     cin>>a>>b;
+
     if (a==1 && b==1)
         cout<<"Bus";
     else if (a==0 && b==1)
         cout<<"Walk";
+    else if (a==1 && b==0)
+        cout<<"Bus";
+    else if (a==0 && b==0)
+        cout<<"Walk";
 
 }
-
